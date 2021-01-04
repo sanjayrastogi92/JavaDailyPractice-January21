fddc2251efa90b2b9962c0a55d6318813b347705
Sanjay Rastogi <sanjayrastogi92@gmail.com>
2021-01-04 23:48:53 +0530

    commit for browser launch using switch

---
+++ src/jan_04/BrowserCallUsingSwitch.java
@@ -0,0 +1,35 @@
+package jan_04;
+
+import java.util.Scanner;
+
+public class BrowserCallUsingSwitch {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter any browser name");
+		String browserName = sc.nextLine();
+		
+		sc.close();
+		System.out.println("You've entered "+browserName+" browser");
+		switch(browserName)
+		{
+		case "chrome":
+			System.out.println("Launching chrome");
+			break;
+			
+		case "firefox":
+			System.out.println("Launching firefox");
+			break;
+			
+		case "IE":
+			System.out.println("Launching IE");
+			break;
+			
+		default:
+			System.out.println("Please enter the valid browser name");
+		}
+
+	}
+
+}
